ae11e9b33cf82f693208e63c8461fe693eaecc9a
Kenta IDA <cinimlm@gmail.com>
2018-01-27 04:47:52 +0900

    Add socket functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from wiolte import LTEModule
 import logging
+import struct
 
 logging.basicConfig(logging.DEBUG)
 
@@ -10,3 +11,7 @@ if m.turn_on_or_reset():
     print('LTE connection is now available.')
     print(m.get_RSSI())
 
+    m.activate('soracom.io', 'sora', 'sora')
+    conn = m.socket_open('beam.soracom.io', 1883, m.SOCKET_TCP)
+    print('Connection to SORACOM Beam = {0}'.format(conn))
+    
--- wiolte.py
+++ wiolte.py
@@ -11,11 +11,21 @@ class WioLTE(object):
 
     def get_comm(self) -> LTEModule:
         return self.__comm
-        
+
+class LTEModuleError(RuntimeError):
+    def __init__(self, message:str):
+        super().__init__(message)
+
 class LTEModule(object):
     "Controls Quectel EC21 LTE Module"
     CR = const(0x0d)
     LF = const(0x0a)
+
+    SOCKET_TCP = const(0)
+    SOCKET_UDP = const(1)
+
+    MAX_CONNECT_ID = const(12)
+    MAX_SOCKET_DATA_SIZE = const(1460)
 
     def __init__(self):
         self.__l = logging.Logger('LTEModule')
@@ -124,26 +134,153 @@ class LTEModule(object):
     
     def get_IMEI(self) -> str:
         "Gets International Mobile Equipment Identity (IMEI)"
-        response = self.execute_comand_single_response(b'AT+GSN')
+        response = self.execute_command_single_response(b'AT+GSN')
         return str(response, 'utf-8') if response is not None else None
     
     def get_IMSI(self) -> str:
         "Gets International Mobile Subscriber Identity (IMSI)"
-        response = self.execute_comand_single_response(b'AT+CIMI')
+        response = self.execute_command_single_response(b'AT+CIMI')
         return str(response, 'utf-8') if response is not None else None
 
     def get_phone_number(self) -> str:
         "Gets phone number (subscriber number)"
-        response = self.execute_comand_single_response(b'AT+CNUM', b'+CNUM:')
+        response = self.execute_command_single_response(b'AT+CNUM', b'+CNUM:')
         return str(response[6:], 'utf-8') if response is not None else None
 
     def get_RSSI(self) -> int:
         "Gets received signal strength indication (RSSI)"
-        response = self.execute_comand_single_response(b'AT+CSQ', b'+CSQ:')
+        response = self.execute_command_single_response(b'AT+CSQ', b'+CSQ:')
+        if response is None:
+            return response
         try:
-            return int(str(response[5:], 'utf-8')) if response is not None else None
+            s = str(response[5:], 'utf-8')
+            rssi, ber = s.split(',', 2)
+            return (int(rssi), int(ber))
         except ValueError:
             return None
+    
+    def activate(self, access_point:str, user:str, password:str) -> bool:
+        self.__l.info("Activating network...")
+        while True:
+            # Read network registration status.
+            response = self.execute_command_single_response(b'AT+CGREG?', b'+CGREG:')
+            if response is None:
+                raise LTEModuleError('Failed to get registration status.')
+            s = str(response, 'utf-8')
+            self.__l.debug('AT+CGREG?:%s', s)
+            n, stat = s.split(',')[:2]
+            if stat == '0' or stat == '4':  # Not registered and not searching (0), or unknown (4).
+                raise LTEModuleError('Invalid registration status.')
+            elif stat == '1' or stat == '5': # Registered.
+                break
+            
+            # Read EPS network registration status
+            response = self.execute_command_single_response(b'AT+CEREG?', b'+CEREG:')
+            if response is None:
+                raise LTEModuleError('Failed to get registration status.')
+            s = str(response, 'utf-8')
+            self.__l.debug('AT+CEREG?:%s', s)
+            n, stat = s.split(',')[:2]
+            if stat == '0' or stat == '4':  # Not registered and not searching (0), or unknown (4).
+                raise LTEModuleError('Invalid registration status.')
+            elif stat == '1' or stat == '5': # Registered.
+                break
+        # Configure TCP/IP contect parameters
+        # contextID,context_type,APN,username,password,authentication
+        # context_type  : IPv4 = 1, IPv4/v6 = 2
+        # authentication: None = 0, PAP = 1, CHAP = 2, PAP or CHAP = 3
+        command = bytes('AT+QICSGP=1,1,"{0}","{1}","{2}",1'.format(access_point, user, password), 'utf-8')
+        if not self.write_command_wait(command, b'OK'):
+            return False
+        # Activate a PDP context
+        if not self.write_command_wait(b'AT+QIACT=1', b'OK'):
+            return False
+        if not self.write_command_wait(b'AT+QIACT?', b'OK'):
+            return False
+        
+        return True
+    
+    def socket_open(self, host:str, port:int, socket_type:int) -> int:
+        assert(host is not None)
+        assert(port is not None and 0 <= port and port <= 65535)
+        if socket_type == LTEModule.SOCKET_TCP:
+            socket_type_name = 'TCP'
+        elif socket_type == LTEModule.SOCKET_UDP:
+            socket_type_name = 'UDP'
+        else:
+            socket_type_name = None
+        assert(socket_type_name is not None)
+
+        
+
+        buffer = bytearray(1024)
+
+        # Read current connections and find unused connection.
+        success, responses = self.execute_command(b'AT+QISTATE?', buffer)
+        if not success:
+            raise LTEModuleError('Failed to get socket status')
+        connect_id_in_use = set()
+        for response in responses:
+            if len(response) < 10 or response[:10] != b'+QISTATE: ': continue
+            s = str(bytes(response[10:]), 'utf-8')
+            self.__l.debug(s)
+            params = s.split(',',1)
+            connect_id = int(params[0])
+            connect_id_in_use.add(connect_id)
+
+        new_connect_id = None
+        for connect_id in range(LTEModule.MAX_CONNECT_ID):
+            if connect_id not in connect_id_in_use:
+                new_connect_id = connect_id
+                break
+        if new_connect_id is None:
+            raise LTEModuleError('No connection resources available.')
+
+        # Open socket.
+        command = bytes('AT+QIOPEN=1,{0},"{1}","{2}",{3}'.format(connect_id, socket_type_name, host, port), 'utf-8')
+        if not self.write_command_wait(command, b'OK'):
+            raise LTEModuleError('Failed to open socket.')
+        if self.wait_response(bytes('+QIOPEN: {0},0'.format(connect_id), 'utf-8')) is None:
+            raise LTEModuleError('Failed to open socket.')
+
+        return connect_id
+    
+    def socket_send(self, connect_id:int, data:bytes, offset:int=0, length:int=None) -> bool:
+        assert(0 <= connect_id and connect_id <= LTEModule.MAX_CONNECT_ID)
+
+        length = len(data) if length is None else length
+        if length == 0:
+            return True
+        assert(length <= LTEModule.MAX_SOCKET_DATA_SIZE)
+
+        command = bytes('AT+QISEND={0},{1}'.format(connect_id, length), 'utf-8')
+        self.write_command(command)
+        if not self.wait_prompt(b'> '):
+            return False
+        mv = memoryview(data)
+        self.__uart.write(mv[offset:offset+length])
+        return self.wait_response(b'SEND OK') is not None
+    
+    def socket_receive(self, connect_id:int, buffer:bytearray, offset:int=0, length:int=None) -> int:
+        assert(0 <= connect_id and connect_id <= LTEModule.MAX_CONNECT_ID)
+
+        length = len(buffer) if length is None else length
+        if length == 0:
+            return True
+        assert(length <= LTEModule.MAX_SOCKET_DATA_SIZE)
+
+        command = bytes('AT+QIRD={0},{1}'.format(connect_id,length), 'utf-8')
+        self.write_command(command)
+        response = self.wait_response(b'+QIRD: ')
+        if response is None:
+            return None
+        actual_length = int(str(response[7:], 'utf-8'))
+        if actual_length == 0:
+            return 0
+        mv = memoryview(buffer)
+        self.__uart.readinto(mv[offset:offset+length], actual_length)
+        return actual_length if self.wait_response(b'OK') is not None else None
+    
     
     def is_busy(self) -> bool:
         return bool(self.__pin_module_status.value())
@@ -208,6 +345,19 @@ class LTEModule(object):
             self.__l.debug("wait_response: response=%s", response[:length])
             if length >= expected_length and response[:expected_length] == expected_response:
                 return response[:length]
+    
+    def wait_prompt(self, expected_prompt:bytes) -> bool:
+        prompt_length = len(expected_prompt)
+        index = 0
+        while True:
+            c = self.__uart.readchar()
+            if c < 0: return False  # Timed out
+            if expected_prompt[index] == c:
+                index += 1
+                if index == prompt_length:
+                    return True
+            else:
+                index = 0
 
     def execute_command(self, command:bytes, response_buffer:bytearray, index:int=0, expected_response_predicate:Callable[[memoryview],bool]=None, expected_response_list:List[bytes]=[b'OK']) -> Tuple[bool, List[memoryview]]:
         assert expected_response_predicate is not None or expected_response_list is not None
@@ -227,7 +377,7 @@ class LTEModule(object):
                 return (True, responses)
             index += length
 
-    def execute_comand_single_response(self, command:bytes, starts_with:bytes=None) -> bytes:
+    def execute_command_single_response(self, command:bytes, starts_with:bytes=None) -> bytes:
         buffer = bytearray(1024)
         result, responses = self.execute_command(command, buffer)
         if not result: return None
